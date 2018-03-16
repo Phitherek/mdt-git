b458a0d490ecd3046d54ff5907c1b292aed38162
Piotr Żurek (Coders'Peak) <piotr@coderspeak.com>
2018-03-16 19:07:19 +0100

    Add documentation, fix load files

---
+++ lib/mdt-git.rb
@@ -0,0 +1,2 @@
+require_relative 'mdt/version'
+require_relative 'mdt/fetchers'
--- lib/mdt/fetchers/git.rb
+++ lib/mdt/fetchers/git.rb
@@ -1,16 +1,32 @@
 require 'mdt-core'
 
 module MDT
+  # A module containing all fetchers
   module Fetchers
+    # A class that implements Git fetchers
     class Git < MDT::Fetchers::Base
+      # A method that defines a key for fetchers class.
+      # Returns:
+      # * "git"
       def self.key
         'git'
       end
 
+      # A method that defines keys for available fetchers.
+      # Returns:
+      # * +["repository"]+
       def self.subkeys
         ['repository']
       end
 
+      # A method that defines how to fetch project contents to a deploy directory with fetchers.
+      # Arguments:
+      # * +key+ - a key identifier of a particular fetcher
+      # * +options+ - options for fetchers as a Hash
+      # Returns:
+      # * Exit code for fetcher +key+
+      # More information:
+      # * See README.md for detailed description of fetchers
       def fetch(key, options = {})
         case key
         when 'repository'
